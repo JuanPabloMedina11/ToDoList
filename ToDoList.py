658ea2123a0af667f9bcdb99417a9ba4586e254a
JuanPabloMedina11 <medinajuanpi11@gmail.com>
2024-12-11 17:56:45 -0700

    Changed name

--- ToDoList.py
+++ ToDoList.py
@@ -10,20 +10,32 @@ a = True
 while(a == True):
 
     answer = input("1:add \n2:remove \n3:close\n")
+    print("")
 
     if answer == "1":
         add = input("Write the task: ")
         toDo.append(add)
+        print("")
+
     elif answer == "2":
+
+        for i in toDo:
+            print(i)
+
         remove = input("Which task do you want to remove: ")
         remove = int(remove) - 1     #Since the list starts from the number 0 the input the person will place will be one higher
         toDo.pop(remove)
+
+        print("")
+
     elif answer == "3":
+
         a = False
+        
     else:
         print("Invalid Input. Please enter 1,2, or 3.")
 
 for i in toDo:
-    print(str(len(i)) +": "+ i )
+    print(i)
 
     
